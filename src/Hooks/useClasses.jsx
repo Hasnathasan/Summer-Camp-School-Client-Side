18d4e6f0925bd9ff620c421832c9749261a821fb
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-30 01:08:10 +0600

    code formated of useClasses hook

--- src/Hooks/useClasses.jsx
+++ src/Hooks/useClasses.jsx
@@ -1,15 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useClasses = () => {
-    const { data: classes = [], isLoading: isClassesLoading, refetch: refetchClasses } = useQuery({
-        queryKey: ['classes'],
-        queryFn: async() => {
-            const res = await fetch("https://summer-camp-server-black.vercel.app/classes")
-            return res.json()
-        },
-      })
-      console.log(classes);
-      return [classes, isClassesLoading, refetchClasses]
+  const {
+    data: classes = [],
+    isLoading: isClassesLoading,
+    refetch: refetchClasses,
+  } = useQuery({
+    queryKey: ["classes"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://summer-camp-server-black.vercel.app/classes"
+      );
+      return res.json();
+    },
+  });
+  console.log(classes);
+  return [classes, isClassesLoading, refetchClasses];
 };
 
 export default useClasses;

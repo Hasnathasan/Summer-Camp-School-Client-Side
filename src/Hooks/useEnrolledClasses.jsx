c807baae3aea534577f9752a014928e2a101e010
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-30 01:40:59 +0600

    Some changes in hooks

--- src/Hooks/useEnrolledClasses.jsx
+++ src/Hooks/useEnrolledClasses.jsx
@@ -3,21 +3,21 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useEnrolledClasses = () => {
-    const { user } = useAuth();
-    const [axiosSecure] = useAxiosSecure()
-    const {
-      data: enrolledClasses,
-      isLoading: isEnrolledClassesLoading,
-      refetch: refetchEnrolledClasses,
-    } = useQuery({
-      queryKey: ["enrolledClasses", user?.email],
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/enrolledclasses/${user?.email}`)
-        return res.data;
-      },
-    });
-  
-    return [enrolledClasses, isEnrolledClassesLoading, refetchEnrolledClasses];
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    data: enrolledClasses,
+    isLoading: isEnrolledClassesLoading,
+    refetch: refetchEnrolledClasses,
+  } = useQuery({
+    queryKey: ["enrolledClasses", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/enrolledclasses/${user?.email}`);
+      return res.data;
+    },
+  });
+
+  return [enrolledClasses, isEnrolledClassesLoading, refetchEnrolledClasses];
 };
 
 export default useEnrolledClasses;

c00377a7d0c0bae05ccc2f7f5daacc4f4a822fb4
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-30 01:04:49 +0600

    Some hooks formated

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,17 +1,16 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  baseURL: 'https://summer-camp-server-black.vercel.app', // Replace with your base URL https://summer-camp-server-black.vercel.app
+  baseURL: "https://summer-camp-server-black.vercel.app", // Replace with your base URL https://summer-camp-server-black.vercel.app
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-
     axiosSecure.interceptors.request.use((config) => {
-      const accessToken = localStorage.getItem('access-token');
+      const accessToken = localStorage.getItem("access-token");
       if (accessToken) {
         config.headers.Authorization = `Bearer ${accessToken}`;
       }
@@ -21,8 +20,11 @@ const useAxiosSecure = () => {
     axiosSecure.interceptors.response.use(
       (response) => response,
       (error) => {
-        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-          navigate('/'); // Redirect to the home page
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
+          navigate("/"); // Redirect to the home page
         }
         return Promise.reject(error);
       }

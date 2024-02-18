d7fba282be9975093d3816c401eeb6a4f7db4dae
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-18 21:54:47 +0600

    more changes in hook

--- src/Hooks/usePamentHistory.jsx
+++ src/Hooks/usePamentHistory.jsx
@@ -3,17 +3,18 @@ import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
 
 const usePamentHistory = () => {
-    const [axiosSecure] = useAxiosSecure();
-    const {user} = useAuth()
-    const { data: paymentHistory = [], isLoading: isPaymentHistoryLoading } = useQuery({
-     queryKey: ['paymentHistory'],
-     queryFn: async() => {
-         const res = await axiosSecure.get(`/payments/${user?.email}`)
-         console.log(res.data);
-         return res.data
-     },
-   })
-   return [paymentHistory, isPaymentHistoryLoading]
+  const [axiosSecure] = useAxiosSecure();
+  const { user } = useAuth();
+  const { data: paymentHistory = [], isLoading: isPaymentHistoryLoading } =
+    useQuery({
+      queryKey: ["paymentHistory"],
+      queryFn: async () => {
+        const res = await axiosSecure.get(`/payments/${user?.email}`);
+        console.log(res.data);
+        return res.data;
+      },
+    });
+  return [paymentHistory, isPaymentHistoryLoading];
 };
 
 export default usePamentHistory;
--- src/Hooks/useUsers.jsx
+++ src/Hooks/useUsers.jsx
@@ -2,15 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useUsers = () => {
-    const [axiosSecure] = useAxiosSecure()
-    const { data: users = [], isLoading: isUsersLoading, refetch } = useQuery({
-        queryKey: ['allusers'],
-        queryFn: async() => {
-            const res = await axiosSecure.get('/users');
-            return res.data
-        },
-      })
-      return [users, isUsersLoading, refetch]
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    data: users = [],
+    isLoading: isUsersLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allusers"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+  return [users, isUsersLoading, refetch];
 };
 
 export default useUsers;

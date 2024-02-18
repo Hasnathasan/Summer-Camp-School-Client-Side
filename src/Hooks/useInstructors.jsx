93c7e5755b8b4f190baac14aabe8721d57e9a242
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-18 22:56:58 +0600

    Changes in useInstructors.jsx hook

--- src/Hooks/useInstructors.jsx
+++ src/Hooks/useInstructors.jsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useInstructors = () => {
-     const { data: intructors, isLoading: isIntructorsLoading } = useQuery({
-            queryKey: ['instructors'],
-            queryFn: async() => {
-                const res = await fetch('https://summer-camp-server-black.vercel.app/instructors');
-                return res.json()
-            },
-          })
-          return [intructors, isIntructorsLoading]
+  const { data: intructors, isLoading: isIntructorsLoading } = useQuery({
+    queryKey: ["instructors"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://summer-camp-server-black.vercel.app/instructors"
+      );
+      return res.json();
+    },
+  });
+  return [intructors, isIntructorsLoading];
 };
 
 export default useInstructors;

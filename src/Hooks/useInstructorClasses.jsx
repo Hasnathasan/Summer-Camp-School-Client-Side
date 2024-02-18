136484d97f0dc91c6cdf54cff6db542be3e30644
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-18 21:56:42 +0600

    useInstructorClasses.jsx formated with prettier

--- src/Hooks/useInstructorClasses.jsx
+++ src/Hooks/useInstructorClasses.jsx
@@ -3,16 +3,19 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useInstructorClasses = () => {
-    const [axiosSecure] = useAxiosSecure()
-    const {user} = useAuth()
-    const { data: intructorClasses, isLoading: isIntructorClassesLoading } = useQuery({
-        queryKey: ['classesByInstructor'],
-        queryFn: async() => {
-            const res = await axiosSecure.get(`/instructorClasses?email=${user?.email}`)
-            return res.data;
-        },
-      })
-      return [intructorClasses, isIntructorClassesLoading]
+  const [axiosSecure] = useAxiosSecure();
+  const { user } = useAuth();
+  const { data: intructorClasses, isLoading: isIntructorClassesLoading } =
+    useQuery({
+      queryKey: ["classesByInstructor"],
+      queryFn: async () => {
+        const res = await axiosSecure.get(
+          `/instructorClasses?email=${user?.email}`
+        );
+        return res.data;
+      },
+    });
+  return [intructorClasses, isIntructorClassesLoading];
 };
 
 export default useInstructorClasses;
